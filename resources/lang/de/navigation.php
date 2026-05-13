a674702ae6269580bd6f6eae349672a0c613eb0a
Misaf <misaf.1990@gmail.com>
2026-05-13 13:05:55 +0330

    feat: add German translations

---
+++ resources/lang/de/navigation.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'activity_log'            => 'Aktivitätsprotokolle',
+    'activity_log_management' => 'Aktivitätsprotokoll-Verwaltung',
+];
---
+++ resources/lang/de/tables.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'batch_uuid'   => 'Batch-UUID',
+    'causer_type'  => 'Verursachertyp',
+    'created_at'   => 'Erstellt am',
+    'event'        => 'Ereignis',
+    'log_name'     => 'Protokollname',
+    'subject_type' => 'Betrefftyp',
+    'updated_at'   => 'Aktualisiert am',
+];
