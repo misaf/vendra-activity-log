aad25ca4bd86d4e974be94128f795051fdbc5de6
Misaf <misaf.1990@gmail.com>
2026-06-05 22:10:32 +0330

    feat: add activity log seed command

---
+++ src/Console/Commands/SeedCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Misaf\VendraActivityLog\Console\Commands;
+
+use Misaf\VendraActivityLog\ActivityLogPlugin;
+use Misaf\VendraActivityLog\Database\Seeders\PermissionPolicySeeder;
+use Misaf\VendraSupport\Console\Commands\BaseSeedCommand;
+
+final class SeedCommand extends BaseSeedCommand
+{
+    protected const string MODULE_NAME = ActivityLogPlugin::ID;
+
+    protected $signature = self::MODULE_NAME . ':seed
+        {tenant : Tenant ID or slug to seed activity log data for}
+        {seeders* : Seeder keys to run. Use "all" or one or more of: permissions}';
+
+    protected $description = 'Seed activity log module data for a tenant';
+
+    /**
+     * @return array<string, class-string>
+     */
+    protected function seeders(): array
+    {
+        return [
+            'permissions' => PermissionPolicySeeder::class,
+        ];
+    }
+}
