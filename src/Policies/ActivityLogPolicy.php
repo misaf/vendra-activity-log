a3603adfcefea28a19855018895ad62cef15caa8
Misaf <misaf.1990@gmail.com>
2026-07-06 13:06:27 +0330

    Clean up module autoload and policies

--- src/Policies/ActivityLogPolicy.php
+++ src/Policies/ActivityLogPolicy.php
@@ -5,66 +5,66 @@ declare(strict_types=1);
 namespace Misaf\VendraActivityLog\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Misaf\VendraActivityLog\Enums\ActivityLogPolicyEnum;
 use Misaf\VendraActivityLog\Models\ActivityLog;
-use Misaf\VendraUser\Models\User;
 
 final class ActivityLogPolicy
 {
     use HandlesAuthorization;
 
-    public function create(User $user): bool
+    public function create(Authorizable $user): bool
     {
-        return $user->can(ActivityLogPolicyEnum::CREATE);
+        return $user->can(ActivityLogPolicyEnum::CREATE->value);
     }
 
-    public function delete(User $user, ActivityLog $activityLog): bool
+    public function delete(Authorizable $user, ActivityLog $activityLog): bool
     {
-        return $user->can(ActivityLogPolicyEnum::DELETE);
+        return $user->can(ActivityLogPolicyEnum::DELETE->value);
     }
 
-    public function deleteAny(User $user): bool
+    public function deleteAny(Authorizable $user): bool
     {
-        return $user->can(ActivityLogPolicyEnum::DELETE_ANY);
+        return $user->can(ActivityLogPolicyEnum::DELETE_ANY->value);
     }
 
-    public function forceDelete(User $user, ActivityLog $activityLog): bool
+    public function forceDelete(Authorizable $user, ActivityLog $activityLog): bool
     {
-        return $user->can(ActivityLogPolicyEnum::FORCE_DELETE);
+        return $user->can(ActivityLogPolicyEnum::FORCE_DELETE->value);
     }
 
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(Authorizable $user): bool
     {
-        return $user->can(ActivityLogPolicyEnum::FORCE_DELETE_ANY);
+        return $user->can(ActivityLogPolicyEnum::FORCE_DELETE_ANY->value);
     }
 
-    public function replicate(User $user, ActivityLog $activityLog): bool
+    public function replicate(Authorizable $user, ActivityLog $activityLog): bool
     {
-        return $user->can(ActivityLogPolicyEnum::REPLICATE);
+        return $user->can(ActivityLogPolicyEnum::REPLICATE->value);
     }
 
-    public function restore(User $user, ActivityLog $activityLog): bool
+    public function restore(Authorizable $user, ActivityLog $activityLog): bool
     {
-        return $user->can(ActivityLogPolicyEnum::RESTORE);
+        return $user->can(ActivityLogPolicyEnum::RESTORE->value);
     }
 
-    public function restoreAny(User $user): bool
+    public function restoreAny(Authorizable $user): bool
     {
-        return $user->can(ActivityLogPolicyEnum::RESTORE_ANY);
+        return $user->can(ActivityLogPolicyEnum::RESTORE_ANY->value);
     }
 
-    public function update(User $user, ActivityLog $activityLog): bool
+    public function update(Authorizable $user, ActivityLog $activityLog): bool
     {
-        return $user->can(ActivityLogPolicyEnum::UPDATE);
+        return $user->can(ActivityLogPolicyEnum::UPDATE->value);
     }
 
-    public function view(User $user, ActivityLog $activityLog): bool
+    public function view(Authorizable $user, ActivityLog $activityLog): bool
     {
-        return $user->can(ActivityLogPolicyEnum::VIEW);
+        return $user->can(ActivityLogPolicyEnum::VIEW->value);
     }
 
-    public function viewAny(User $user): bool
+    public function viewAny(Authorizable $user): bool
     {
-        return $user->can(ActivityLogPolicyEnum::VIEW_ANY);
+        return $user->can(ActivityLogPolicyEnum::VIEW_ANY->value);
     }
 }
