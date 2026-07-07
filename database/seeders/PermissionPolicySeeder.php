14c2b885cce97a1488cf28b791a7907bb823ca87
Misaf <misaf.1990@gmail.com>
2026-07-08 00:11:37 +0330

    Apply pending module updates

--- database/seeders/PermissionPolicySeeder.php
+++ database/seeders/PermissionPolicySeeder.php
@@ -6,21 +6,11 @@ namespace Misaf\VendraActivityLog\Database\Seeders;
 
 use Misaf\VendraActivityLog\ActivityLogPlugin;
 use Misaf\VendraActivityLog\Enums\ActivityLogPolicyEnum;
-use Misaf\VendraSupport\Concerns\RequiresCurrentTenant;
 use Misaf\VendraSupport\Database\Seeders\PermissionPolicySeeder as BasePermissionPolicySeeder;
 
 final class PermissionPolicySeeder extends BasePermissionPolicySeeder
 {
-    use RequiresCurrentTenant;
-
     protected const string MODULE_NAME = ActivityLogPlugin::ID;
-
-    public function run(): void
-    {
-        $tenant = $this->currentTenant();
-
-        $this->seedPermissionPolicies($tenant->getKey());
-    }
 
     /**
      * @return list<string>
